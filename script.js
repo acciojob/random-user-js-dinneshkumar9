a57a31305b28d1aa66e7b1de7925a49d4f0db221
dinneshkumar9 <dinneshkumar9@gmail.com>
2023-02-18 14:30:52 +0530

    done1

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
-//your code here
-const getUserButton = document.getElementById('getUser');
-const userPhoto = document.getElementById('user-photo');
-const userName = document.getElementById('user-name');
+const userPhoto = document.getElementById("user-photo");
+const userName = document.getElementById("user-name");
 const ageButton = document.querySelector('button[data-attr="age"]');
 const emailButton = document.querySelector('button[data-attr="email"]');
 const phoneButton = document.querySelector('button[data-attr="phone"]');
-const ageInfo = document.getElementById('age');
-const emailInfo = document.getElementById('email');
-const phoneInfo = document.getElementById('phone');
-const additionalInfo = document.getElementById('additional-info');
+const ageInfo = document.getElementById("age");
+const emailInfo = document.getElementById("email");
+const phoneInfo = document.getElementById("phone");
+const additionalInfo = document.getElementById("additional-info");
 
 function displayUserInfo(userData) {
-    
   userPhoto.src = userData.picture.large;
   userName.textContent = `${userData.name.first} ${userData.name.last}`;
+}
+
+function displayAgeInfo(userData) {
+  additionalInfo.innerHTML = "";
   ageInfo.textContent = `Age: ${userData.dob.age}`;
+  additionalInfo.appendChild(ageInfo);
+}
+
+function displayEmailInfo(userData) {
+  additionalInfo.innerHTML = "";
   emailInfo.textContent = `Email: ${userData.email}`;
+  additionalInfo.appendChild(emailInfo);
+}
+
+function displayPhoneInfo(userData) {
+  additionalInfo.innerHTML = "";
   phoneInfo.textContent = `Phone: ${userData.phone}`;
+  additionalInfo.appendChild(phoneInfo);
 }
 
 function getUser() {
-  fetch('https://randomuser.me/api/')
-    .then(response => response.json())
-    .then(data => {
+  fetch("https://randomuser.me/api/")
+    .then((response) => response.json())
+    .then((data) => {
       const userData = data.results[0];
       displayUserInfo(userData);
+      ageButton.addEventListener("click", function () {
+        displayAgeInfo(userData);
+      });
+      emailButton.addEventListener("click", function () {
+        displayEmailInfo(userData);
+      });
+      phoneButton.addEventListener("click", function () {
+        displayPhoneInfo(userData);
+      });
     });
 }
 
-getUserButton.addEventListener('click', getUser);
-
-ageButton.addEventListener('click', function() {
-  additionalInfo.innerHTML = '';
-  additionalInfo.appendChild(ageInfo);
-});
-
-emailButton.addEventListener('click', function() {
-  additionalInfo.innerHTML = '';
-  additionalInfo.appendChild(emailInfo);
-});
-
-phoneButton.addEventListener('click', function() {
-  additionalInfo.innerHTML = '';
-  additionalInfo.appendChild(phoneInfo);
-});
+getUser();
+const getUserr = document.getElementById('getUser');
+getUserr.addEventListener('click', getUser);
